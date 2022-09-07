105a47120dcb70f43b30ce6263310218e284fdac
Dmitrii <pinochet37@mail.ru>
2022-09-07 11:56:24 +0300

    added Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+GetDashbord(num);
+
+void GetDashbord(int num)
+{
+    int count = 1;
+    while(count <= Math.Abs(num))
+    {
+        Console.WriteLine($"Куб числа {count} = {Math.Pow(count, 3)}");
+        count++;
+    }
+}
