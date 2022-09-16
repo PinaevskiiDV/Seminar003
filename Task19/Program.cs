e43ed66aab15f00d3f05086f59d0bfdbf46a4d06
Dmitrii <pinochet37@mail.ru>
2022-09-16 14:35:29 +0300

    added Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int ReverseNumber(int num)
+{
+    int reverse = 0;
+    while (num > 0)
+    {
+        reverse = reverse * 10 + num % 10;
+        num /= 10;
+    }    
+    return reverse;
+}
+int reverse = ReverseNumber(number);
+if (reverse == number)
+{
+    Console.WriteLine("Данное число является палиндромом");
+}
+else
+{
+    Console.WriteLine("Данное число не является палиндромом");
+}
